ff4accadccc09ed2d15c6dc9aa07fdd685002bf3
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-02 22:47:39 +0630

    mask image

---
+++ question_2_3.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageDraw, ImageFilter, ImageChops
+
+
+im1 = Image.open('Question2/foreground.jpg')
+
+
+background_img = Image.open('Question2/background.jpg').resize(im1.size)
+
+
+mask = Image.open('Question2/mask.png').resize(im1.size)
+
+
+mask = mask.convert("RGBA")
+
+newImage = []
+for data in mask.getdata():
+    if data[:3] == (0, 255, 0):
+        newImage.append((255, 255, 255, 0))
+    else:
+        newImage.append(data)
+
+mask.putdata(newImage)
+
+im = Image.composite(im1, background_img, mask)
+
+im.save("Question2/result3.jpg")
+im.show()
